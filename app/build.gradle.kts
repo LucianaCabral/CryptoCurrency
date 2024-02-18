2f910f74e08f42364cc08b02a33c1d03d0f84921
LucianaCabral <eng.luciana.cabral@gmail.com>
2024-02-18 10:05:13 -0300

    :heavy_plus_sign: build:[CPA-1] Fixed: setup gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,7 @@
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinAndroid)
-    alias(libs.plugins.hiltAndroidPlugin)
     alias(libs.plugins.kotlinParcelize)
     kotlin("kapt")
 }
@@ -11,77 +10,4 @@ apply("$rootDir/plugins/android-build.gradle")
 
 android {
     namespace = "br.leeloo.coinpaprika"
-//    compileSdk = 34
-
-//    defaultConfig {
-//        applicationId = "br.leeloo.coinpaprika"
-//        minSdk = 24
-//        targetSdk = 34
-//        versionCode = 1
-//        versionName = "1.0"
-//
-//        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-//        vectorDrawables {
-//            useSupportLibrary = true
-//        }
-//
-//        buildFeatures {
-//            compose = true
-//        }
-//        composeOptions {
-//            kotlinCompilerExtensionVersion = "1.4.3"
-//        }
-//    }
-//
-//    buildTypes {
-//        release {
-//            isMinifyEnabled = false
-//            proguardFiles(
-//                getDefaultProguardFile("proguard-android-optimize.txt"),
-//                "proguard-rules.pro"
-//            )
-//        }
-//    }
-//    compileOptions {
-//        sourceCompatibility = JavaVersion.VERSION_17
-//        targetCompatibility = JavaVersion.VERSION_17
-//    }
-//    kotlinOptions {
-//        jvmTarget = "17"
-//    }
-//    buildFeatures {
-//        compose = true
-//    }
-//    composeOptions {
-//        kotlinCompilerExtensionVersion = "1.5.1"
-//    }
-//    packaging {
-//        resources {
-//            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-//        }
-//    }
-}
-
-dependencies {
-
-    // Libs
-    implementation(libs.core.ktx)
-    implementation(libs.lifecycle.runtime.ktx)
-    implementation(libs.activity.compose)
-    implementation(platform(libs.compose.bom))
-    implementation(libs.ui)
-    implementation(libs.ui.graphics)
-    implementation(libs.ui.tooling.preview)
-    implementation(libs.material3)
-
-    // Test
-    testImplementation(libs.junit)
-
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
-    androidTestImplementation(platform(libs.compose.bom))
-
-    androidTestImplementation(libs.ui.test.junit4)
-    debugImplementation(libs.ui.tooling)
-    debugImplementation(libs.ui.test.manifest)
 }
